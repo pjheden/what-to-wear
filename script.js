1fd21e1a2bdd9b3de8ef90d324b0d59f028191aa
pjheden <julianheden@gmail.com>
2016-11-06 14:55:42 +0100

    Add configures for voice response

--- script.js
+++ script.js
@@ -56,8 +56,16 @@ function stopRecognition() {
 
 //voice response
 function voiceResponse(text){
-  var msg = new SpeechSynthesisUtterance(text);
-  window.speechSynthesis.speak(msg);
+  var msg = new SpeechSynthesisUtterance();
+  var voices = window.speechSynthesis.getVoices();
+  msg.voice = voices[10]; // Note: some voices don't support altering params
+  msg.voiceURI = 'native';
+  msg.volume = 1; // 0 to 1
+  msg.rate = 1; // 0.1 to 10
+  msg.pitch = 2; //0 to 2
+  msg.text = text;
+  msg.lang = 'en-US';
+  speechSynthesis.speak(msg);
 }
 
 //Setup function
