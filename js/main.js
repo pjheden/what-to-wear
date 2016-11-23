e8f20e056fb3d3232f6ff95150f28d67209a97e1
pjheden <julianheden@gmail.com>
2016-11-23 13:49:02 +0100

    Set a max for number of fetch weather tries

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
+var weatherCounter;
 jQuery(document).ready(function($) {
+    weatherCounter = 0;
     weather_action();
-    if(isMobileUser()){
-      setMobileStyling();
-    }else{
-      setDesktopStyling();
+    if (isMobileUser()) {
+        setMobileStyling();
+    } else {
+        setDesktopStyling();
     }
 });
 
-function isMobileUser(){
-  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    return true;
-  }
-  return false;
+function isMobileUser() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return true;
+    }
+    return false;
 }
 
 function weather_action() {
+    if(weatherCounter > 9){
+      return;
+    }
     var weatherPromise = getWeather();
     weatherPromise.then(function(weather) {
             var cels = Math.round(parseFloat(weather.feel) * 100) / 100;
@@ -22,6 +27,7 @@ function weather_action() {
         },
         function(error) {
             console.error(error);
+            weatherCounter++;
             weather_action();
         });
 }
@@ -31,9 +37,9 @@ function suggestClothes(temp) {
     var clothing;
     var clothInt = parseInt(kNearestNeighbours(temp));
     clothing = getCloth(clothInt);
-    var response = 'It feels like ' + temp + ' *C in '+getCity()+', '+getCountry()+', you should wear ' + clothing +'.';
+    var response = 'It feels like ' + temp + ' *C in ' + getCity() + ', ' + getCountry() + ', you should wear ' + clothing + '.';
     setResponse(response);
-    setClothImg( getClothesImg(clothInt), clothing);
+    setClothImg(getClothesImg(clothInt), clothing);
 }
 
 //Updates the response p element
@@ -42,35 +48,35 @@ function setResponse(val) {
 }
 
 function setClothImg(imgPath, imgDesc) {
-  $('#clothImg').attr('src', imgPath);
-  $('#clothImg').attr('alt', imgDesc);
+    $('#clothImg').attr('src', imgPath);
+    $('#clothImg').attr('alt', imgDesc);
 }
 
 function setMobileStyling() {
-  /*
+    /*
 
 
-  */
-  $('.container').css('width','100%');
-  // $('h1, h2, h3, h4').css('font-size', '5vmax');
-  // $('p').css('font-size', '1.5vmax');
-  // $('button').css('width', '5vmax');
-  // $('button').css('height', '5vmax');
-  // $('input, select').css('width', '20vmax');
-  // $('input, select').css('height', '5vmax');
-  // $('input, select').css('font-size', '1.5vmax');
+    */
+    $('.container').css('width', '100%');
+    // $('h1, h2, h3, h4').css('font-size', '5vmax');
+    // $('p').css('font-size', '1.5vmax');
+    // $('button').css('width', '5vmax');
+    // $('button').css('height', '5vmax');
+    // $('input, select').css('width', '20vmax');
+    // $('input, select').css('height', '5vmax');
+    // $('input, select').css('font-size', '1.5vmax');
 
-  var fileref = document.createElement("link");
-  fileref.setAttribute("rel", "stylesheet");
-  fileref.setAttribute("type", "text/css");
-  fileref.setAttribute("href", "styles/mobile.css");
-  document.getElementsByTagName("head")[0].appendChild(fileref);
+    var fileref = document.createElement("link");
+    fileref.setAttribute("rel", "stylesheet");
+    fileref.setAttribute("type", "text/css");
+    fileref.setAttribute("href", "styles/mobile.css");
+    document.getElementsByTagName("head")[0].appendChild(fileref);
 }
 
-function setDesktopStyling(){
-  var fileref = document.createElement("link");
-  fileref.setAttribute("rel", "stylesheet");
-  fileref.setAttribute("type", "text/css");
-  fileref.setAttribute("href", "styles/desktop.css");
-  document.getElementsByTagName("head")[0].appendChild(fileref);
+function setDesktopStyling() {
+    var fileref = document.createElement("link");
+    fileref.setAttribute("rel", "stylesheet");
+    fileref.setAttribute("type", "text/css");
+    fileref.setAttribute("href", "styles/desktop.css");
+    document.getElementsByTagName("head")[0].appendChild(fileref);
 }
